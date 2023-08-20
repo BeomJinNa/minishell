03b2d2fec68ca03ae7fb7656d5152fd6e028e61a
windowdong11 <windowdong11@naver.com>
2023-08-21 01:10:50 +0900

    단일 빌트인 명령 실행 시 fork하지 않도록 수정

--- srcs/exec/exec.c
+++ srcs/exec/exec.c
@@ -6,7 +6,7 @@
 /*   By: dowon <dowon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/25 21:23:29 by dowon             #+#    #+#             */
-/*   Updated: 2023/08/18 13:17:37 by dowon            ###   ########.fr       */
+/*   Updated: 2023/08/20 23:44:07 by dowon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@
 
 void	exec_command(char **command)
 {
-	char	*exec_path;
+	// char	*exec_path;
 
 	if (ft_strncmp(command[0], "echo", 5) == 0)
 		exit(builtin_echo(command + 1));
@@ -41,26 +41,23 @@ void	exec_command(char **command)
 		exit(builtin_env(command + 1));
 	else if (ft_strncmp(command[0], "exit", 5) == 0)
 		exit(builtin_exit(command + 1));
-	exec_path = hashtable_get("PATH", get_hashtable(0));
-	if (exec_path == NULL)
-	{
-		printf("%s : command not found\n", command[0]);
-		exit(127);
-	}
-	execve(exec_path,
-		command, NULL);
+	// exec_path = hashtable_get("PATH", get_hashtable(0));
+	// if (exec_path == NULL)
+	// {
+	// 	printf("%s : command not found\n", command[0]);
+	// 	exit(127);
+	// }
+	// execve(exec_path, command, get_hashtable_arr(get_hashtable(0)));
+	execvp(command[0], command);
 }
 
 int	execute_child(t_command command, int *pipes, int idx)
 {
-	printf("exec_child\n");
 	if (open_redirections(command.redirections,
 			readpipe_at(pipes, idx), writepipe_at(pipes, idx + 1)))
 		return (-1);
-	printf("redirections_opened\n");
 	if (dup_pipes(pipes, idx))
 		return (-1);
-	printf("pipes_duplicated\n");
 	exec_command(command.command);
 	close_rw_pipes(pipes, idx);
 	return (-1);
@@ -167,8 +164,26 @@ int	execute_commands(t_command *commands, int size)
 
 	if (pipes == NULL)
 		return (-1);
-	if (size == 1 && is_builtin(commands[0].command))
-		result = run_builtin(commands[0].command);
+	if (size == 1)
+	{
+		if (is_builtin(commands[0].command))
+		{
+			int in = dup(STDOUT_FILENO);
+			int out = dup(STDIN_FILENO);
+			if (open_redirections(commands[0].redirections,
+					readpipe_at(pipes, 0), writepipe_at(pipes, 1)))
+				return (-1);
+			dup2(in, STDIN_FILENO);
+			dup2(out, STDOUT_FILENO);
+			result = run_builtin(commands[0].command);
+		}
+		else
+		{
+			if (fork_n_execute(commands, pipes, 0, size))
+				return (-1);
+			result = wait_all(size);
+		}
+	}
 	else
 	{
 		idx = 0;
