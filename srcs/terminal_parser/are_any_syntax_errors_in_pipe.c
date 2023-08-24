4631d1451f2f4b7b61db915afc8e7388f1a2ec34
BeomJin Na <bjinn94@gmail.com>
2023-08-24 16:32:48 +0900

    FEAT: Add pipe syntax checker to check first and end character of str

--- srcs/terminal_parser/are_any_syntax_errors_in_pipe.c
+++ srcs/terminal_parser/are_any_syntax_errors_in_pipe.c
@@ -6,32 +6,43 @@
 /*   By: bena <bena@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/29 16:41:12 by bena              #+#    #+#             */
-/*   Updated: 2023/08/24 16:03:55 by bena             ###   ########.fr       */
+/*   Updated: 2023/08/24 16:30:31 by bena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
 static int	check_character_sequence(const char **str, char c);
+static int	check_str(const char *ptr, char c, int *flag);
 
 int	are_any_syntax_errors_in_pipe(const char *str)
 {
+	if (str == NULL)
+		return (0);
 	return (check_character_sequence(&str, '|'));
 }
 
 static int	check_character_sequence(const char **str, char c)
 {
 	const char	*ptr;
-	int			in_brace;
-	int			in_double_brace;
 	int			flag;
 
-	if (str == NULL || *str == NULL)
-		return (0);
+	ptr = *str;
+	if (*ptr == c)
+		return (1);
+	flag = 0;
+	if (check_str(ptr, c, &flag))
+		return (1);
+	return (0);
+}
+
+static int	check_str(const char *ptr, char c, int *flag)
+{
+	int			in_brace;
+	int			in_double_brace;
+
 	in_brace = 0;
 	in_double_brace = 0;
-	flag = 0;
-	ptr = *str;
 	while (*ptr)
 	{
 		if (*ptr == '\'' && in_double_brace == 0)
@@ -40,17 +51,16 @@ static int	check_character_sequence(const char **str, char c)
 			in_double_brace ^= 1;
 		if (in_brace == 0 && in_double_brace == 0)
 		{
+			if (*ptr == c && *flag == 1)
+				return (1);
 			if (*ptr == c)
-			{
-				if (flag == 1)
-					return (1);
-				else
-					flag = 1;
-			}
+				*flag = 1;
 			else
-				flag = 0;
+				*flag = 0;
 		}
 		ptr++;
 	}
+	if (*flag == 1)
+		return (1);
 	return (0);
 }
