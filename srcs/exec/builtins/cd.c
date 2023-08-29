e21fa6a1e725acf46b67f748c1b42d351737ae33
Donggyu Won <dowon@c2r5s1.42seoul.kr>
2023-08-29 16:39:18 +0900

    cd에서 PWD, OLDPWD 환경변수 변경하도록 추가

--- srcs/exec/builtins/cd.c
+++ srcs/exec/builtins/cd.c
@@ -6,11 +6,12 @@
 /*   By: dowon <dowon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/21 22:43:42 by dowon             #+#    #+#             */
-/*   Updated: 2023/08/29 14:49:58 by dowon            ###   ########.fr       */
+/*   Updated: 2023/08/29 16:37:33 by dowon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtins.h"
+#include "hash.h"
 #include "libft.h"
 #include <unistd.h>
 #include <stdio.h>
@@ -18,16 +19,29 @@
 int	builtin_cd(char **args)
 {
 	const size_t	argc = count_args(args);
+	char			*buffer;
 
 	if (argc == 0)
 	{
 		printf("cd without arguments are not supported\n");
 		return (1);
 	}
+	buffer = getcwd(NULL, 0);
 	if (chdir(args[0]) == -1)
 	{
 		printf("%s: No such file or directory\n", args[0]);
 		return (1);
+	}
+	if (buffer != NULL)
+	{
+		hashtable_addkey("OLDPWD", buffer, get_hashtable(0));
+		free(buffer);
+	}
+	buffer = getcwd(NULL, 0);
+	if (buffer != NULL)
+	{
+		hashtable_addkey("PWD", buffer, get_hashtable(0));
+		free(buffer);
 	}
 	return (0);
 }
